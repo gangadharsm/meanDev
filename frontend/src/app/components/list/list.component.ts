3fc7973667593f7e376f0eddc7357a841377db10
Gangadhara <gangadhara@bootminds.com>
2018-07-07 08:55:57 +0530

    issueService injucted in list component

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IssueService } from '../../services/issue.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private issueService: IssueService) {
+    this.issueService.getIssues().subscribe(res=>{
+      console.log(res)
+    })
+   }
 
   ngOnInit() {
   }
