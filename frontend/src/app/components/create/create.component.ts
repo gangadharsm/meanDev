cb787ba6a54f5d27e0f36c7fd342b72dc16668f2
Gangadhara <gangadhara@bootminds.com>
2018-07-09 13:16:34 +0530

    create UI

--- frontend/src/app/components/create/create.component.ts
+++ frontend/src/app/components/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IssueService } from '../../services/issue.service';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  createForm: FormGroup;
+
+  constructor(private issueService: IssueService, private fb: FormBuilder, private router: Router) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: ''
+    });
+  }
 
   ngOnInit() {
+  }
+
+  addIssue(title, responsible, description, severity) {
+    this.issueService.addIssue(title, responsible, description, severity).subscribe(() => {
+      this.router.navigate(['/list']);
+    });
   }
 
 }
